2060f5799845d619a378cbf204fe7541db8fd02f
Joshua Clark <joshuadclark89@gmail.com>
2025-11-01 18:17:29 -0500

    Implemented PostHog integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    cacheComponents: true,
+    experimental: {
+        turbopackFileSystemCacheForDev: true
+    },
+    /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/ingest/static/:path*",
+                destination: "https://us-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/ingest/:path*",
+                destination: "https://us.i.posthog.com/:path*",
+            },
+        ];
+    },
+    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
