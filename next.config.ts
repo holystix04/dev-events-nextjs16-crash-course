c72ad5175170893cbdd9f9a96f650b76b1c658dd
holystix04 <8400967+holystix04@users.noreply.github.com>
2025-11-01 21:22:07 -0500

    Branches/prettier (#3)
    * initial prettier config
    
    * prettier import updates
    
    * add husky and lint-staged

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,24 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    cacheComponents: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true
-    },
-    /* config options here */
-    async rewrites() {
-        return [
-            {
-                source: "/ingest/static/:path*",
-                destination: "https://us-assets.i.posthog.com/static/:path*",
-            },
-            {
-                source: "/ingest/:path*",
-                destination: "https://us.i.posthog.com/:path*",
-            },
-        ];
-    },
-    skipTrailingSlashRedirect: true,
+  cacheComponents: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
