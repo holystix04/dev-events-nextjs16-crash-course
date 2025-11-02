84293fa1c6454b130c76a911ef0259b506732793
holystix04 <8400967+holystix04@users.noreply.github.com>
2025-11-02 00:47:08 -0500

    branches/api-routes (#4)
    * mid-routing
    
    * re-based wit formatting
    
    * implement api routes, server actions and caching, and the event details page
    
    * Update app/events/[slug]/page.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * use slug for event key
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import { cacheLife } from 'next/cache';
+
 import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
-import { events } from '@/lib/constants';
+import type { IEvent } from '@/database';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  'use cache';
+  cacheLife('hours');
+
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -17,11 +27,13 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.slug}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,41 @@
+﻿'use server';
+
+import type { IEvent } from '@/database';
+import { Event } from '@/database';
+import connectDB from '@/lib/mongodb';
+
+export const getSimilarEventsBySlug = async (
+  slug: string
+): Promise<IEvent[]> => {
+  try {
+    await connectDB();
+
+    const event = await Event.findOne({ slug });
+    const events = await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags },
+    }).lean();
+    return events.map(
+      (event) =>
+        ({
+          id: event._id,
+          title: event.title,
+          slug: event.slug,
+          description: event.description,
+          overview: event.overview,
+          date: event.date,
+          time: event.time,
+          location: event.location,
+          mode: event.mode,
+          agenda: event.agenda,
+          audience: event.audience,
+          tags: event.tags,
+          organizer: event.organizer,
+          image: event.image,
+        }) as IEvent
+    );
+  } catch (error) {
+    console.error('Error fetching similar events:', error);
+    return [];
+  }
+};
