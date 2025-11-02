ce7d5f8ef6623af235c4d320f3cd693983d70d40
Joshua Clark <joshuadclark89@gmail.com>
2025-11-01 21:26:10 -0500

    re-based wit formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,38 @@
-import ExploreBtn from "@/components/ExploreBtn";
-import EventCard from "@/components/EventCard";
-import {IEvent} from "@/database";
+import EventCard from '@/components/EventCard';
+import ExploreBtn from '@/components/ExploreBtn';
+import type { IEvent } from '@/database';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
-    return (
-        <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conferences - All in One Place</p>
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences - All in One Place
+      </p>
 
       <ExploreBtn />
 
-            <div className='mt-20 space-y-7'>
-                <h3>Featured Events</h3>
-                <ul className='events'>
-                    {events && events.length > 0 && events.map((event: IEvent) => (
-                        <li key={event.title}>
-                            <EventCard {... event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    )
-}
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Page;
--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,32 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    cacheComponents: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-            }
-        ],
-    },
-    /* config options here */
-    async rewrites() {
-        return [
-            {
-                source: "/ingest/static/:path*",
-                destination: "https://us-assets.i.posthog.com/static/:path*",
-            },
-            {
-                source: "/ingest/:path*",
-                destination: "https://us.i.posthog.com/:path*",
-            },
-        ];
-    },
-    skipTrailingSlashRedirect: true,
+  cacheComponents: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
